01cbd0e4577a1373c62c3002cb911f73eba25092
Samuel Park <sampark@redhat.com>
2026-06-12 16:30:08 -0400

    Added the hostnetwork scc policy.

--- create_dev_admin.sh
+++ create_dev_admin.sh
@@ -33,6 +33,9 @@ oc apply -f <(sed "s/<username>/$USERNAME/g" namespace.yml)
 # Apply anyuid to bypass SCC.
 oc adm policy add-scc-to-user anyuid -z default -n $USERNAME
 
+# Allow hostNetwork for UCX-based RDMA workloads (vLLM/NIXL, OpenMPI).
+oc adm policy add-scc-to-user hostnetwork -z default -n $USERNAME
+
 # Apply edit role to the user to allow them to create resources in their namespace.
 oc adm policy add-role-to-user edit "$IDENTITY" -n $USERNAME
 
